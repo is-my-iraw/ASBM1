d79b862a6b8f93ddfdf8f45fc1258e1918744e93
Admin <Admin@ADMIN>
2021-12-23 16:52:51 +0700

    Add project files.

---
+++ ASBM1/Areas/Identity/Data/ASBM1Context.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ASBM1.Models;
+
+namespace ASBM1.Data;
+
+public class ASBM1Context : IdentityDbContext<IdentityUser>
+{
+    public ASBM1Context(DbContextOptions<ASBM1Context> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+
+    public DbSet<ASBM1.Models.Product> Product { get; set; }
+
+    public DbSet<ASBM1.Models.Category> Category { get; set; }
+}
---
+++ ASBM1/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace ASBM1.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public int CategoryID { get; set; }
+        public Category? Category { get; set; }
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        public string? Name { get; set; }
+        [DataType(DataType.Currency)]
+        public int Price { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string? Unit { get; set; }
+    }
+}
